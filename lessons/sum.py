273fd721c0304d204c86e651190e9030ebf0ee57
Nicholas Mann <nmann@unc.edu>
2023-03-02 14:36:52 -0500

    sum challenge questions

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,10 +1,11 @@
-"""Example function for unit tests"""
+"""Example function for unit tests."""
+
+__author__ = "730486147"
+
 
 def sum(xs: list[float]) -> float:
-    """return sum of all elements in xs"""
+    """return sum of all elements in xs."""
     sum_total: float = 0.0
-    idx: int = 0
-    while idx < len(xs):
-        sum_total += xs[idx]
-        idx += 1
+    for i in range(len(xs)):
+        sum_total += xs[i]
     return sum_total
